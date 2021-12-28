ef0998c1d1e97cd5c12a9544a0db313fc3188884
kk-shindo <cubkich@gmail.com>
2021-12-28 13:27:19 +0900

    refactor: App functionコンポーネントに変更

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 // import { Link } from 'react-router-dom'
 
 import Tweets from './components/Tweets'
@@ -14,161 +14,157 @@ import User from './json/user.json'
 * - DB連携
 */
 
-export default class App extends Component {
-  constructor() {
-    super()
-    this.user = User[1]
-    this.state = {
-      userId: null,
-      tweet: '',
-      tweetList: JSON.parse(localStorage.getItem('tweetList')) || [],
-      favorites: JSON.parse(localStorage.getItem('favorites')) || []
-    }
+export default function App () {
+  const user = User[1]
+  const [tweet, setTweet] = useState('')
+  const [tweetList, setTweetList] = useState([])
+  const [favorites, setFavorites] = useState([])
 
-    this.submitHandler = this.submitHandler.bind(this)
-    this.changeHandler = this.changeHandler.bind(this)
-    this.clickHandler = this.clickHandler.bind(this)
-    this.onClickFavorite = this.onClickFavorite.bind(this)
-  }
+  useEffect(() => {
+    setTweetList(JSON.parse(localStorage.getItem('tweetList')) || [])
+    setFavorites(JSON.parse(localStorage.getItem('favorites')) || [])
+  }, [])
 
-  componentDidMount() {
-    this.setState({userId: this.user.id})
-  }
+  useEffect(() => {
+    localStorage.setItem('tweetList', JSON.stringify(tweetList))
+    // setTweetList((current) => {
+    //   return sortTweetList(current)
+    // })
+  }, [tweetList])
 
-  submitHandler(e) {
-    const { userId, tweet, tweetList } = this.state
+  const submitHandler = (e) => {
     const date = new Date()
     const timeStamp = Math.floor(date.getTime() / 1000)
 
     e.preventDefault()
     if (tweet) {
-      this.setState({
-        tweet: '',
-        tweetList: tweetList.concat({
+      setTweet('')
+      setTweetList((current) => {
+        return current.concat({
           id: timeStamp,
-          userId: userId,
+          userId: user.id,
           date: date.toLocaleString(),
           tweet: tweet
         })
-      }, () => {
-        e.target.elements.tweet.value = ''
-
-        localStorage.setItem('tweetList', JSON.stringify(this.state.tweetList))
-        window.alert('ツイートしました')
       })
+
+      e.target.elements.tweet.value = ''
+      // localStorage.setItem('tweetList', JSON.stringify(tweetList))
+      // window.alert('ツイートしました')
     }
   }
 
-  changeHandler(e) {
+  const changeHandler = (e) => {
     const target = e.target
 
     if (target.value.length < 140) {
-      this.setState({[target.name]: target.value})
+      switch (target.name) {
+        case 'tweet':
+          setTweet(target.value)
+          break
+        default:
+          break
+      }
     } else {
       window.alert('Over 140 characters.')
     }
   }
 
-  clickHandler(todo) {
-    const { tweetList } = this.state
-    this.setState({
-      tweetList: tweetList.filter((x) => x !== todo)
-    }, () => {
-      localStorage.setItem('tweetList', JSON.stringify(this.state.tweetList))
+  const clickHandler = (todo) => {
+    setTweetList((current) => {
+      return current.filter((x) => x !== todo)
     })
+    localStorage.setItem('tweetList', JSON.stringify(tweetList))
   }
 
   /** お気に入りを押したときの挙動 */
-  onClickFavorite(tweet) {
-    let { favorites } = this.state
-
+  const onClickFavorite = (tweet) => {
     if (favorites.includes(tweet.id)) {
       const index = favorites.indexOf(tweet.id)
       favorites.splice(index, 1)
     } else {
       if (favorites.length > 0) {
-        favorites = favorites.concat(tweet.id)
+        setFavorites((current) => {
+          current.concat(tweet.id)
+        })
       } else {
-        favorites = [tweet.id]
+        setFavorites([tweet.id])
       }
     }
 
-    this.setState({ favorites }, () => {
-      localStorage.setItem('favorites', JSON.stringify(this.state.favorites))
+    setFavorites(favorites)
+    localStorage.setItem('favorites', JSON.stringify(favorites))
+  }
+
+  const sortTweetList = (_tweetList) => {
+    return _tweetList.sort((a, b) => {
+      return new Date(a.date) < new Date(b.date) ? 1 : -1
     })
   }
 
-  render() {
-    const { tweetList, favorites } = this.state
+  return (
+    <div className="App l-container">
+      <div className="l-container__left">
+        <nav className="c-nav1">
+          <a href="/" className="c-nav1__logo">
+            <img src="/img/logo.svg" alt="" className="c-nav1__logo__img" />
+          </a>
+          <ul className="c-list2">
+            <li className="c-list2__item">
+              <a href="/" className="c-list2__link">
+                <img src="/img/icon/profile.svg" alt="" className="c-list2__link__img" />
+                <span className="c-list2__link__txt">プロフィール</span>
+              </a>
+            </li>
+            <li className="c-list2__item">
+              <a href="/" className="c-list2__link">
+                <img src="/img/icon/profile.svg" alt="" className="c-list2__link__img" />
+                <span className="c-list2__link__txt">プロフィール</span>
+              </a>
+            </li>
+            <li className="c-list2__item">
+              <a href="/" className="c-list2__link">
+                <img src="/img/icon/profile.svg" alt="" className="c-list2__link__img" />
+                <span className="c-list2__link__txt">プロフィール</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
 
-    tweetList.sort((a, b) => {
-      return new Date(a.date) < new Date(b.date) ? 1 : -1
-    })
-
-    return (
-      <div className="App l-container">
-        <div className="l-container__left">
-          <nav className="c-nav1">
-            <a href="/" className="c-nav1__logo">
-              <img src="/img/logo.svg" alt="" className="c-nav1__logo__img" />
-            </a>
-            <ul className="c-list2">
-              <li className="c-list2__item">
-                <a href="/" className="c-list2__link">
-                  <img src="/img/icon/profile.svg" alt="" className="c-list2__link__img" />
-                  <span className="c-list2__link__txt">プロフィール</span>
-                </a>
-              </li>
-              <li className="c-list2__item">
-                <a href="/" className="c-list2__link">
-                  <img src="/img/icon/profile.svg" alt="" className="c-list2__link__img" />
-                  <span className="c-list2__link__txt">プロフィール</span>
-                </a>
-              </li>
-              <li className="c-list2__item">
-                <a href="/" className="c-list2__link">
-                  <img src="/img/icon/profile.svg" alt="" className="c-list2__link__img" />
-                  <span className="c-list2__link__txt">プロフィール</span>
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-
-        <div className="l-container__center">
-          <form
-            className="c-form1"
-            onSubmit={this.submitHandler}
-          >
-            <Icon1 src={this.user.icon} />
-            <Textarea
-              change={this.changeHandler}
+      <div className="l-container__center">
+        <form
+          className="c-form1"
+          onSubmit={submitHandler}
+        >
+          <Icon1 src={user.icon} />
+          <Textarea
+            change={changeHandler}
+          />
+          <footer className="c-form1__footer">
+            <Btn1
+              text="ツイート"
             />
-            <footer className="c-form1__footer">
-              <Btn1
-                text="ツイート"
-              />
-            </footer>
-          </form>
-          <div className="c-list1">
-          {tweetList.length > 0 && tweetList.map((tweet, i) => (
+          </footer>
+        </form>
+        <div className="c-list1">
+          {(tweetList || []).length > 0 && sortTweetList(tweetList).map((tweet, i) => (
             <Tweets
               key={i}
               tweet={tweet}
-              onClick={() => {/* this.clickHandler(tweet) */}}
+              onClick={() => { clickHandler(tweet) }}
               isFavorite={favorites.length ? (favorites).includes(tweet.id) : false}
-              onClickFavorite={this.onClickFavorite}
+              onClickFavorite={onClickFavorite}
             />
           ))}
-          </div>
-        </div>
-
-        <div className="l-container__left">
-          <div className="">
-
-          </div>
         </div>
       </div>
-    )
-  }
+
+      <div className="l-container__right">
+        <div className="">
+
+        </div>
+      </div>
+    </div>
+  )
 }
